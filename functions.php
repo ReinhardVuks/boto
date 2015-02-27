8af8ce855d935f95a6d241027ff771f37eae48a8
ollikas <olevabel@gmail.com>
2015-02-27 15:40:46 +0200

    test dbconnect

--- functions.php
+++ functions.php
@@ -1,21 +1,22 @@
 <?php
 function dbconnect(){
-	$servername="localhost";
-$username="vistor";
-$password="secure_guest";
-$dbname="test";
+	$servername="eu-cdbr-azure-north-b.cloudapp.net";
+	$username="be6bf743499900";
+	$password="2569fe2a";
+	$dbname="botodb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-if($conn->connect_error){
-    die("connection failed:" . $conn->connect_error);
-}
-$sql="SELECT * FROM user";
-if ($conn->query($sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if($conn->connect_error){
+    	die("connection failed:" . $conn->connect_error);
+	}
+	$sql="SELECT * FROM user";
+	if ($conn->query($sql)) {
+    	echo "New record created successfully";
+	} 
+	else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 
 }
 dbconnect();
