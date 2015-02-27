2a6747e849981921ccd45510636c34a9eabff45e
gerrupoiss <gerrerth.kaur@gmail.com>
2015-02-27 11:53:45 +0200

    Login

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+
+        <link rel="stylesheet" type="text/css" href="style/login.css">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+
+        <title>BOTO</title>
+    </head>
+    <body>
+	    <div class="login">
+            <?php echo '<a href="' . $login_url . '">LOGIN</a>';?>
+        </div>
+    </body>
+</html>
